a046af6595bd32a08009f11363e7cb4a97e39900
Björn <bjoernwe@users.noreply.github.com>
2025-02-23 20:19:35 +0100

    Print SFA sentences

--- src/dho_analysis/plot_slowness.py
+++ src/dho_analysis/plot_slowness.py
@@ -3,6 +3,7 @@ import numpy as np
 from polars import Series
 
 from sklearn.decomposition import PCA
+from sksfa import SFA
 
 from dho_analysis.calc_message_embeddings import add_message_embeddings
 from dho_analysis.calc_sentences import explode_msg_to_sentences
@@ -19,28 +20,37 @@ def main():
 
 def plot_slowness(
         author: str = "Linda ”Polly Ester” Ö",
-        model: str = "all-MiniLM-L6-v2",
-        time_aggregate: str = "1w",
-        pca_components: int = 20,
+        model: str = "all-mpnet-base-v2",
+        time_aggregate: str = "1d",
+        pca_components: int = 50,
 ):
 
     df0 = load_practice_logs_for_author(author=author)
+    df0 = df0.sort("date")
 
     df_sen = explode_msg_to_sentences(df=df0)
     df_sen = add_message_embeddings(df=df_sen, model=model)
 
-    pca = PCA(n_components=pca_components, random_state=SEED)
-    pca.fit(np.array(df_sen["embedding"]))
-
     df_agg = aggregate_messages_by_time(df=df0, time_aggregate=time_aggregate)
     df_agg = add_message_embeddings(df=df_agg, model=model)
 
-    old_embeddings: Series = df_agg["embedding"]
-    new_embeddings: Series = Series("embedding", pca.transform(np.array(old_embeddings)))
-    df_agg = df_agg.with_columns(new_embeddings)
+    pca = PCA(n_components=pca_components, random_state=SEED)
+    pca.fit(np.array(df_sen["embedding"]))
 
-    df_agg = add_sfa_columns(df_agg)
-    plt.plot(df_agg.select(["date"]), df_agg.select(["SFA_0"]))
+    df_sen = df_sen.with_columns(Series("embedding", pca.transform(np.array(df_sen["embedding"]))))
+    df_agg = df_agg.with_columns(Series("embedding", pca.transform(np.array(df_agg["embedding"]))))
+
+    sfa = SFA(n_components=1, random_state=SEED)
+    sfa.fit(np.array(df_agg["embedding"]))
+
+    df_sen = df_sen.with_columns(Series("SFA", sfa.transform(np.array(df_sen["embedding"]))))
+    df_agg = df_agg.with_columns(Series("SFA", sfa.transform(np.array(df_agg["embedding"]))))
+
+    for s in df_sen.sort("SFA_0")["msg"].to_list()[:10]: print(s)
+    print("\n...\n")
+    for s in df_sen.sort("SFA_0")["msg"].to_list()[-10:]: print(s)
+
+    plt.plot(df_agg.select(["date"]), df_agg.select(["SFA"]))
     plt.show()
 
 

3cfbcea53796aaac61448cc8bf3322ec3ac89903
Björn <bjoernwe@users.noreply.github.com>
2025-02-23 15:13:32 +0100

    Fix practice logs

--- src/dho_analysis/load_practice_logs_for_author.py
+++ src/dho_analysis/load_practice_logs_for_author.py
@@ -1,3 +1,5 @@
+from typing import Set
+
 import polars as pl
 
 from polars import DataFrame
@@ -11,9 +13,22 @@ def main():
 
 
 def load_practice_logs_for_author(author: str) -> DataFrame:
-    return read_dho_messages().filter(
+    dho_messages = read_dho_messages()
+    practice_threads_by_author = _get_practice_thread_ids_by_author(df=dho_messages, author=author)
+    return dho_messages.filter(
         pl.col("category").eq("PracticeLogs"),
         pl.col("author").eq(author),
+        pl.col("thread_id").is_in(practice_threads_by_author),
+    )
+
+
+def _get_practice_thread_ids_by_author(df: DataFrame, author: str) -> Set[int]:
+    return set(
+        df.filter(
+            pl.col("category").eq("PracticeLogs"),
+            pl.col("author").eq(author),
+            pl.col("is_first_in_thread"),
+        ).get_column("thread_id")
     )
 
 

3a3124cc9ffbf976b46aff741b2b9a04c01322c3
Björn <bjoernwe@users.noreply.github.com>
2025-03-03 20:55:39 +0100

    Add paths for Colab

--- src/config.py
+++ src/config.py
@@ -9,8 +9,13 @@ from polars import DataFrame
 SEED = 0
 
 PROJECT_PATH: Path = Path(__file__).parent.parent
+
 CACHE_DIR: str = str(PROJECT_PATH.joinpath("cache"))
+#CACHE_DIR: str = str(PROJECT_PATH.parent.joinpath("drive/MyDrive/Datasets/joblib-cache"))
+
 DATA_PATH: Path = PROJECT_PATH.joinpath("data")
+#DATA_PATH: Path = PROJECT_PATH.parent.joinpath(Path("drive/MyDrive/Datasets/dho-scrapy"))
+
 
 memory = Memory(location=CACHE_DIR)
 

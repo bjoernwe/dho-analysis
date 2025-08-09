d0598735d444d88cc200e14a8c697cac346b94f8
Björn <bjoernwe@users.noreply.github.com>
2025-08-09 21:12:05 +0200

    Add comments

--- src/functions/utils.py
+++ src/functions/utils.py
@@ -6,14 +6,19 @@ from polars import DataFrame, Series
 
 
 def map_numpy_column(df: DataFrame, column_name: str, f: Callable[[np.ndarray], np.ndarray], target_columns: str = None) -> DataFrame:
+
+    # map array column
     x = np.array(df[column_name])
     y = f(x)
 
+    # replace source column
     if target_columns is None:
         return df.with_columns(Series(column_name, y))
 
+    # ... or create new columns
     n_columns = y.shape[1]
     result = DataFrame(df)
     for i in range(n_columns):
         result = result.with_columns(Series(f"{target_columns}_{i}", y[:, i]))
+
     return result

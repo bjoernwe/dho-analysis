c8fac35140582e06882e4c82e27ee9aedc5be004
Björn <bjoernwe@users.noreply.github.com>
2026-06-19 21:51:03 +0200

    Extend message

--- kotlin/src/main/kotlin/data/Message.kt
+++ kotlin/src/main/kotlin/data/Message.kt
@@ -1,19 +1,8 @@
 package data
 
-import org.jetbrains.kotlinx.dataframe.annotations.ColumnName
 import org.jetbrains.kotlinx.dataframe.annotations.DataSchema
 
 @DataSchema
-interface Message {
-    @ColumnName("msg_id")
-    val msgId: Long
-    @ColumnName("thread_id")
-    val threadId: Long
-    val date: String
-    @ColumnName("is_first_in_thread")
-    val isFirstInThread: Boolean
-    val category: String
-    val author: String
-    val title: String
-    val msg: String?
+interface Message: RawMessage {
+    val threadAuthor: String
 }

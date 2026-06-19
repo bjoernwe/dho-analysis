2f8c5f7812e3ce61862561029dbc8ad045ee430f
Björn <bjoernwe@users.noreply.github.com>
2026-06-19 20:14:01 +0200

    Remove unused code

--- kotlin/src/main/kotlin/hello/Greeter.kt
+++
@@ -1,7 +0,0 @@
-package hello
-
-fun greet(name: String): String = "Hello, $name!"
-
-class Greeter(private val greeting: String = "Hello") {
-    fun greet(name: String): String = "$greeting, $name!"
-}

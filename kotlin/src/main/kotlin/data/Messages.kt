70bb61b40dab11727413b6dd9f9ca32ee5c7d78f
Björn <bjoernwe@users.noreply.github.com>
2026-06-20 21:42:20 +0200

    Extract method

--- kotlin/src/main/kotlin/data/Messages.kt
+++ kotlin/src/main/kotlin/data/Messages.kt
@@ -11,11 +11,16 @@ import models.SentenceSplitter
 import java.io.File
 
 fun readMessages(splitter: SentenceSplitter, path: String = "data/messages.jsonl"): DataFrame<Message> {
-    val json = readLinesAsJsonArray(path)
-    val rawMessages = DataFrame.readJsonStr(json).convertTo<RawMessage>()
+    val rawMessages = readRawMessages(path)
     val threadAuthors = rawMessages.filter { it.isFirstInThread }.map { it.threadId to it.author }.toMap()
+    val json = readLinesAsJsonArray(path)
     val messages = DataFrame.readJsonStr(json).convertTo<Message> { fill { threadAuthor }.with { threadAuthors[threadId] ?: "n/a" }; fill { sentences }.with { splitter.split(msg ?: "") } }
     return messages
+}
+
+private fun readRawMessages(path: String): DataFrame<RawMessage> {
+    val json = readLinesAsJsonArray(path)
+    return DataFrame.readJsonStr(json).convertTo<RawMessage>()
 }
 
 private fun readLinesAsJsonArray(path: String): String {

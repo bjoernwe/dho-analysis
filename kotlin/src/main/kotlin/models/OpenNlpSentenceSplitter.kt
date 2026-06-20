87ce5ba6c015f4e02800cc234dfae40f5fb7416d
Björn <bjoernwe@users.noreply.github.com>
2026-06-20 21:25:40 +0200

    Add default

--- kotlin/src/main/kotlin/models/OpenNlpSentenceSplitter.kt
+++ kotlin/src/main/kotlin/models/OpenNlpSentenceSplitter.kt
@@ -3,9 +3,10 @@ package models
 import opennlp.tools.sentdetect.SentenceDetectorME
 import opennlp.tools.sentdetect.SentenceModel
 import java.nio.file.Path
+import kotlin.io.path.Path
 import kotlin.io.path.inputStream
 
-class OpenNlpSentenceSplitter(modelPath: Path) : SentenceSplitter {
+class OpenNlpSentenceSplitter(modelPath: Path = Path("models/OpenNLP/opennlp-en-ud-ewt-sentence-1.3-2.5.4.bin")) : SentenceSplitter {
 
     private val detector = SentenceDetectorME(modelPath.inputStream().use { SentenceModel(it) })
 

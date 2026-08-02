add891c8fd490f5ba79749b383cc0c86eed17754
Björn <bjoernwe@users.noreply.github.com>
2026-08-02 16:51:42 +0200

    Fix scripts

--- python/src/config.py
+++ python/src/config.py
@@ -13,7 +13,7 @@ PROJECT_PATH: Path = Path(__file__).parent.parent
 CACHE_DIR: str = str(PROJECT_PATH.joinpath("cache"))
 #CACHE_DIR: str = str(PROJECT_PATH.parent.joinpath("drive/MyDrive/Datasets/joblib-cache"))
 
-DATA_PATH: Path = PROJECT_PATH.joinpath("data")
+DATA_PATH: Path = PROJECT_PATH.joinpath("../data")
 #DATA_PATH: Path = PROJECT_PATH.parent.joinpath(Path("drive/MyDrive/Datasets/dho-scrapy"))
 
 
